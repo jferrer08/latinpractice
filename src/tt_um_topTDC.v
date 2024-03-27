cddf1345432fb6e9e03c74a58c7bf22ecb07671b
Juan Ferrer <hellpawn@gmail.com>
2024-03-26 21:58:30 -0600

    Update tt_um_topTDC.v

--- src/tt_um_topTDC.v
+++ src/tt_um_topTDC.v
@@ -17,8 +17,13 @@ module tt_um_topTDC (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uio_out = 0;
+  //assign uio_oe  = 0;
+
+    topTDC U1 (.clk(clk), .stop(uio_in[7]), .reset(uio_in[6]), .tx(uio_out[1]), .eot(uio_out[0]));
+    assign uio_eo = 8'b00000011;
+    assign uio_out[7:2] = 6'b000000;
+    assign uo_out[7:0] = 8'b00000000;
 
 endmodule
